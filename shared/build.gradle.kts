5de68c50ba178b75f1d4ca0460ca0b503139d295
Sandeep <sandeep.dev.4010@gmail.com>
2026-07-13 14:01:11 +0530

    Added dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.androidMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -37,6 +38,8 @@ kotlin {
     sourceSets {
         androidMain.dependencies {
             implementation(libs.compose.uiToolingPreview)
+            implementation(libs.ktor.client.okhttp)
+            implementation(libs.ktor.client.logging)
         }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
@@ -47,6 +50,17 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization)
+            implementation(libs.kotlinx.serialization.json)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            implementation(libs.ktor.client.logging)
+
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
