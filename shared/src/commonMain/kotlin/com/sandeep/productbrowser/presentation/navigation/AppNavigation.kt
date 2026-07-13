5957cfc321ff7fb70782d7e3b6f7a26c901b50b7
Sandeep <sandeep.dev.4010@gmail.com>
2026-07-13 23:21:05 +0530

    update  fully supported across all platforms.

--- shared/src/commonMain/kotlin/com/sandeep/productbrowser/presentation/navigation/AppNavigation.kt
+++ shared/src/commonMain/kotlin/com/sandeep/productbrowser/presentation/navigation/AppNavigation.kt
@@ -1,63 +1,49 @@
 package com.sandeep.productbrowser.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import com.sandeep.productbrowser.presentation.product.ProductDetailScreen
 import com.sandeep.productbrowser.presentation.product.ProductListScreen
 import com.sandeep.productbrowser.presentation.productdetail.ProductDetailViewModel
 import com.sandeep.productbrowser.presentation.productlist.ProductListViewModel
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object ProductListRoute
+
+@Serializable
+data class ProductDetailRoute(val productId: Int)
 
 @Composable
 fun AppNavigation(
-
     productListViewModel: ProductListViewModel,
     productDetailViewModel: ProductDetailViewModel
-
 ) {
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = Screen.ProductList.route
-
+        startDestination = ProductListRoute
     ) {
 
-        composable(
-            route = Screen.ProductList.route
-
-        ) {
-
+        composable<ProductListRoute> {
             ProductListScreen(
                 viewModel = productListViewModel,
-
-                onProductClick = {
-                    navController.navigate(
-                        Screen.ProductDetail.createRoute(it)
-                    )
+                onProductClick = { id ->
+                    navController.navigate(ProductDetailRoute(id))
                 }
             )
         }
 
-        composable(
-            route = Screen.ProductDetail.route,
-
-            arguments = listOf(
-                navArgument("productId") {
-                    type = NavType.StringType
-                }
-            )
-        ) {
-
-            val productId =
-                it.arguments?.getString("productId")?.toIntOrNull() ?: 0
+        composable<ProductDetailRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<ProductDetailRoute>()
 
             ProductDetailScreen(
                 viewModel = productDetailViewModel,
-                productId = productId,
+                productId = route.productId,
                 onBack = {
                     navController.popBackStack()
                 }
