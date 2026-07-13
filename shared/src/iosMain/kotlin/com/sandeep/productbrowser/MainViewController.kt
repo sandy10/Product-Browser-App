f3d375afb18e09d229606b7d5d42bbef6d3ed3c5
Sandeep <sandeep.dev.4010@gmail.com>
2026-07-13 22:58:24 +0530

    Updated MainViewController for ios build success

--- shared/src/iosMain/kotlin/com/sandeep/productbrowser/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/sandeep/productbrowser/MainViewController.kt
@@ -1,5 +1,16 @@
 package com.sandeep.productbrowser
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import com.sandeep.productbrowser.core.di.AppModule
+import com.sandeep.productbrowser.presentation.App
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val productListViewModel = remember { AppModule.provideProductListViewModel() }
+    val productDetailViewModel = remember { AppModule.provideProductDetailViewModel() }
+
+    App(
+        productListViewModel = productListViewModel,
+        productDetailViewModel = productDetailViewModel
+    )
+}
