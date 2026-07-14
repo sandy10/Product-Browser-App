87b6599a8502cfc8e1861388206c051c48c64872
Sandeep <sandeep.dev.4010@gmail.com>
2026-07-14 11:13:30 +0530

    remove un-used file

--- shared/src/commonMain/kotlin/com/sandeep/productbrowser/GreetingUtil.kt
+++
@@ -1,6 +0,0 @@
-package com.sandeep.productbrowser
-
-import com.sandeep.productbrowser.core.common.Constants
-
-fun sayHello(to: String): String =
-    "${Constants.Strings.HELLO}$to!"
--- shared/src/commonMain/kotlin/com/sandeep/productbrowser/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.sandeep.productbrowser
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
